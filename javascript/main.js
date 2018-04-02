3f74527ebb04f1bf9010775a094d8d8e91d9323e
vitya potapchuk <vityapotapchuk@gmail.com>
2018-04-02 13:41:54 +0300

    add js folder

---
+++ javascript/main.js
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>UIIE</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+	<title>UIIE</title>
+</head>
+<body>	
+	<header>
+		<section>
+			<div class="container">
+				<div class="shapka clearfix">
+					<div class="logo">
+						<img src="img/logo.png" alt="UIIE">
+					</div>
+					<nav>
+						<ul class="menu">
+							<li>
+								<a href="http://uiie.com.ua/">О нас</a>
+							</li>
+							<li>
+								<a href="#">вузы партнеры</a>
+							</li>
+							<li>
+								<a href="#">приём студентов</a>
+							</li>
+							<li>
+								<a href="#">новости</a>
+							</li>
+							<li>
+								<a href="#">контакты</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="titles">
+					<h1>Ukrainian Institute <br> of International Education</h1>
+				</div>
+			</div>
+		</section>
+	</header>
+	<section>
+			<div class="container">
+				<div class="inform">
+					<form action="" class="search">
+						<input placeholder="Поиск по сайту" type="search">
+					</form>					
+				</div>
+				<div class="button">
+				</div>
+				<div class="news">
+					<div class="colons">
+						<div class="left-col">
+							<a href="http://www.ubs.edu.ua/"><div class="but1">убд</div></a>
+							<a href="https://kneu.edu.ua/"><div class="but1">кнэу</div></a>
+							<a href="http://uiie.com.ua/"><div class="but1">условия приема</div></a>
+						</div>
+						<div class="center-col">
+							<div class="but-center">
+								<h2>о нас</h2>
+								<p>
+									   В этом году Украина и Китай отмечают 25 лет установления 
+										дипломатических отношений и выходят на новый, 
+										стратегический уровень взаимодействия, который 
+										предусматривает реализацию совместных проектов, в т.ч. и 
+										в области образования. Премьер-министр  Владимир 
+										Гройсман  публично заявил, что 2019 год будет объявлен 
+										Годом Китая в Украине . Это открываются новые возможности
+										для углубления сотрудничества , а также реализации 
+										совместных проектов и инициатив в области образования.
+										    «Украинский институт международного образования» 
+										совместно с Государственным предприятием «Украинский 
+										государственный центр международного образования» 
+										Министерства образования и науки Украины уже начала 
+										работу по подготовке таких инициатив. В частности 
+										организован образовательный процесс  по  подготовке 
+										соискателей высшего образования из числа граждан 
+										Китайской Народной Республики по обмену на 
+										коммерческой основе  в Глобальной образовательной сети, 
+										куда в настоящее время входят : Киевский национальный 
+										экономическом университете имени Вадима Гетьмана
+										( Украина), Університетот Редлендс (Каліфорнія, США), 
+										Университет банковского дела ( Украина), Краковский 
+										Экономический Университет (Республика Польша), 
+										Университет Миколаса Ромериса (Литва), Высшая школа 
+										Менеджмента (Испания),  Киевский медицинский университет. 
+										Сеть постоянно расширяется.
+       
+								</p>
+							</div>
+						</div>
+						<div class="right-col">
+							<div class="but2">
+								<a href="http://uiie.com.ua/dogovor-o-sotrudnichestve/">
+								<h3>Подписание договора</h3>
+								<img src="img/new1.png" alt="">	
+								</a>
+									
+							</div>
+							<div class="but2">
+								<a href="http://www.ubs.edu.ua/index.php/ua/navchannya">
+								<h3>УБД</h3>
+								<img src="img/new2.png" alt="">	
+								</a>
+								
+							</div>
+							<div class="but2">
+								<a href="http://uiie.com.ua/digital-technologies-in-the-classroom/">
+								<h3>Сотрудничество</h3>
+								<img src="img/new3.png" alt="">	
+								</a>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				<img src="img/blank_china.jpg" alt="" width="50%" id="blank_china">
+				<a href="https://docs.google.com/forms/d/e/1FAIpQLSdzwfDI4pBXVl9M20h2AaPQHu86vNzzPa4gQhMU2nSwawAgrg/viewform"><div class="button3">Заявка-анкета на обучение</div></a>
+			</div>
+	</section>
+	<section>
+		<!--Контейнер для лого партнёров/ start -->
+			<div class="container clearfix">
+				<div class="title-partners clearfix">
+					<h2>Партнеры глобальной образовательной сети УИМО</h2>
+				</div>
+					<ul class="partners">
+						<li><img src="img/par-1.png" alt=""></li>
+						<li><img src="img/par-2.png" alt=""></li>
+						<li><img src="img/par-3.png" alt=""></li>
+						<li><img src="img/par-4.png" alt=""></li>
+						<li><img src="img/par-5.png" alt=""></li>
+						<li><img src="img/par-6.png" alt=""></li>
+					</ul>
+
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="forma">
+					<form action="" mathod="post">
+						
+					</form>
+					<label for="email-field">E-mail</label><br>
+					<input type="text" name="email-field" id="email-field"><br><br>
+					<textarea name="comment" id="comment-field" cols="30" rows="10">Комментарий</textarea><br><br>
+					<input type="submit" value="Отправить" class="submit">
+				</div>
+			</div>
+		</section>
+</body>
+</html>
